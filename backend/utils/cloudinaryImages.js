c1a5aa1f9442ff57a75ac79e6048a4cf93a32955
Matt Henely <matt.henely8@gmail.com>
2026-06-26 23:00:14 -0700

    Wire product images to Cloudinary
    Replace the placehold.co placeholders with real Cloudinary URLs. Item
    public_ids are mapped in cloudinaryImages.js (generated from the account
    via the Admin API, keyed by "<Category>|<Item>"); shopData builds each
    imageUrl with f_auto,q_auto,w_800.
    
    Handles the account's dynamic-folders naming: Soccer (Sports/Hobbies) and
    Barcelona (Sports/Travel) share a filename but are disambiguated by folder,
    and two upload typos (valenci-2, gerard-3) are aliased to the right slots.
    All 180 image URLs verified to return 200 and render.

---
+++ backend/utils/cloudinaryImages.js
@@ -0,0 +1,306 @@
+// Generated from the Cloudinary account via the Admin API.
+// Maps "<Category>|<Item>" to the ordered public_ids of its gallery images.
+// Regenerate (scripts) if you re-upload or rename assets.
+
+module.exports = {
+  "Sports|Soccer": [
+    "soccer-1_iztwgy",
+    "soccer-2_wyflvg",
+    "soccer-3_eo56v3"
+  ],
+  "Sports|Hockey": [
+    "hockey-1_xhllo4",
+    "hockey-2_pu8q0y",
+    "hockey-3_ph6g9y"
+  ],
+  "Sports|Aussie Rules Football": [
+    "aussie-rules-football-1_zw0jk6",
+    "aussie-rules-football-2_rinj3p",
+    "aussie-rules-football-3_tj0u9c"
+  ],
+  "Sports|Liverpool": [
+    "liverpool-1_zxfunn",
+    "liverpool-2_p2wwrq",
+    "liverpool-3_h0xpfd"
+  ],
+  "Sports|Barcelona": [
+    "barcelona-1_tvk3on",
+    "barcelona-2_wj9omv",
+    "barcelona-3_chdm9d"
+  ],
+  "Sports|Valencia": [
+    "valencia-1_igjcgw",
+    "valenci-2_w8vtpm",
+    "valencia-3_wmjux5"
+  ],
+  "Sports|Blues": [
+    "blues-1_bdye1e",
+    "blues-2_zdcads",
+    "blues-3_fk3rld"
+  ],
+  "Sports|Cubs": [
+    "cubs-1_kceeeg",
+    "cubs-2_crhjpw",
+    "cubs-3_mli4py"
+  ],
+  "Sports|Gerrard": [
+    "gerrard-1_yrf0r7",
+    "gerrard-2_a7nc8g",
+    "gerard-3_kimexq"
+  ],
+  "Sports|Messi": [
+    "messi-1_il4bid",
+    "messi-2_btx2hi",
+    "messi-3_h5nssk"
+  ],
+  "Sports|Sidney Crosby": [
+    "sidney-crosby-1_imviqf",
+    "sidney-crosby-2_fv16ks",
+    "sidney-crosby-3_tft1ys"
+  ],
+  "Sports|Firmino": [
+    "firmino-1_dc1svl",
+    "firmino-2_mtqpsu",
+    "firmino-3_iicxip"
+  ],
+  "Food|Italian": [
+    "italian-1_ycmlvf",
+    "italian-2_sxqz8p",
+    "italian-3_dqrvzs"
+  ],
+  "Food|Sushi": [
+    "sushi-1_easwxb",
+    "sushi-2_wc1mwv",
+    "sushi-3_cahfes"
+  ],
+  "Food|Burger": [
+    "burger-1_vdqufn",
+    "burger-2_ahunhq",
+    "burger-3_ak6y3j"
+  ],
+  "Food|BBQ": [
+    "bbq-1_zz076n",
+    "bbq-2_blw7ps",
+    "bbq-3_h1xuo6"
+  ],
+  "Food|Thai": [
+    "thai-1_o9zyxs",
+    "thai-2_yoakjp",
+    "thai-3_xciljj"
+  ],
+  "Food|Hawaiian": [
+    "hawaiian-1_ofxzso",
+    "hawaiian-2_lxkjdq",
+    "hawaiian-3_ryvpbg"
+  ],
+  "Food|Mexican": [
+    "mexican-1_vcvx8y",
+    "mexican-2_yonjlm",
+    "mexican-3_bispcz"
+  ],
+  "Food|LeChon": [
+    "lechon-1_h3lscs",
+    "lechon-2_jz9lng",
+    "lechon-3_lxwvix"
+  ],
+  "Food|EEM": [
+    "eem-1_hi8ggl",
+    "eem-2_j3jhvg",
+    "eem-3_mnm9wu"
+  ],
+  "Food|Pappy's": [
+    "pappys-1_egkqnv",
+    "pappys-2_irmwkn",
+    "pappys-3_bvzzkz"
+  ],
+  "Food|Cave of Cheese": [
+    "cave-of-cheese-1_yky83b",
+    "cave-of-cheese-2_pzpngn",
+    "cave-of-cheese-3_g6vdf9"
+  ],
+  "Food|Imo's": [
+    "imos-1_a2tamb",
+    "imos-2_bjs30d",
+    "imos-3_gu4q0c"
+  ],
+  "Travel|Paris": [
+    "paris-1_cizuaf",
+    "paris-2_lce6sm",
+    "paris-3_nhnnwp"
+  ],
+  "Travel|Rome": [
+    "rome-1_htqz58",
+    "rome-2_wmyesx",
+    "rome-3_oacg4x"
+  ],
+  "Travel|Machu Picchu": [
+    "machu-picchu-1_yokyuc",
+    "machu-picchu-2_wjaloy",
+    "machu-picchu-3_rgt5my"
+  ],
+  "Travel|Sydney": [
+    "sydney-1_p8i4vw",
+    "sydney-2_nlrlia",
+    "sydney-3_hdimaj"
+  ],
+  "Travel|Hawaii": [
+    "hawaii-1_mrf7ly",
+    "hawaii-2_ohzakp",
+    "hawaii-3_cr9eke"
+  ],
+  "Travel|BVI": [
+    "bvi-1_wop10i",
+    "bvi-2_qerc92",
+    "bvi-3_wrzpyo"
+  ],
+  "Travel|Barcelona": [
+    "barcelona-1_i4kpp3",
+    "barcelona-2_kogdt9",
+    "barcelona-3_bb8i7v"
+  ],
+  "Travel|Tokyo": [
+    "tokyo-1_tpa85y",
+    "tokyo-2_rx8sqs",
+    "tokyo-3_cyngs3"
+  ],
+  "Travel|London": [
+    "london-1_k8dubb",
+    "london-2_bmbuun",
+    "london-3_qyugh0"
+  ],
+  "Travel|Ireland": [
+    "ireland-1_fnszk3",
+    "ireland-2_yljyzf",
+    "ireland-3_ovcze3"
+  ],
+  "Travel|Munich": [
+    "munich-1_brpzhc",
+    "munich-2_oibgyg",
+    "munich-3_qtjkmd"
+  ],
+  "Travel|Buenos Aires": [
+    "buenos-aires-1_ofpzlb",
+    "buenos-aires-2_td6a4g",
+    "buenos-aires-3_kubz0i"
+  ],
+  "Hobbies|Soccer": [
+    "soccer-1_gadhzu",
+    "soccer-2_vmetok",
+    "soccer-3_iemu7r"
+  ],
+  "Hobbies|Sailing": [
+    "sailing-1_mwg2ws",
+    "sailing-2_xwc9cv",
+    "sailing-3_oq7lmz"
+  ],
+  "Hobbies|Writing": [
+    "writing-1_yloy5l",
+    "writing-2_ns8wcw",
+    "writing-3_i40cjx"
+  ],
+  "Hobbies|Stand Up": [
+    "stand-up-1_cayebq",
+    "stand-up-2_txz6ac",
+    "stand-up-3_ze8yvb"
+  ],
+  "Hobbies|Reading": [
+    "reading-1_b9pmnw",
+    "reading-2_q7qewy",
+    "reading-3_avtglk"
+  ],
+  "Hobbies|Hiking": [
+    "hiking-1_xq22vf",
+    "hiking-2_bssshp",
+    "hiking-3_hp8ain"
+  ],
+  "Hobbies|Cooking": [
+    "cooking-1_uycl4f",
+    "cooking-2_wqwtsw",
+    "cooking-3_gqihd3"
+  ],
+  "Hobbies|Working Out": [
+    "working-out-1_ebhq5f",
+    "working-out-2_swcxzl",
+    "working-out-3_ciaxpw"
+  ],
+  "Hobbies|Surfing": [
+    "surfing-1_gtfa6k",
+    "surfing-2_z5eha5",
+    "surfing-3_y7bdcz"
+  ],
+  "Hobbies|Salsa Dancing": [
+    "salsa-dancing-1_hsn4ox",
+    "salsa-dancing-2_ln0ln9",
+    "salsa-dancing-3_pnwkrc"
+  ],
+  "Hobbies|Ice Skating": [
+    "ice-skating-1_o88kcy",
+    "ice-skating-2_c7fcdu",
+    "ice-skating-3_kvzhpr"
+  ],
+  "Hobbies|Coding": [
+    "coding-1_c11ggq",
+    "coding-2_j4rusj",
+    "coding-3_t2l2wr"
+  ],
+  "Books & Music|The Count of Monte Cristo": [
+    "the-count-of-monte-cristo-1_g9kwpv",
+    "the-count-of-monte-cristo-2_wadjxf",
+    "the-count-of-monte-cristo-3_smk3zp"
+  ],
+  "Books & Music|A Place of Greater Safety": [
+    "a-place-of-greater-safety-1_aprxfk",
+    "a-place-of-greater-safety-2_nm0x2f",
+    "a-place-of-greater-safety-3_dtyytb"
+  ],
+  "Books & Music|The Realm of the Elderlings": [
+    "the-realm-of-the-elderlings-1_owwov4",
+    "the-realm-of-the-elderlings-2_zq5zmg",
+    "the-realm-of-the-elderlings-3_jzzl7a"
+  ],
+  "Books & Music|Lord of the Rings": [
+    "lord-of-the-rings-1_uccodm",
+    "lord-of-the-rings-2_h0dzg7",
+    "lord-of-the-rings-3_hvip0m"
+  ],
+  "Books & Music|To Kill A Mockingbird": [
+    "to-kill-a-mockingbird-1_eusumc",
+    "to-kill-a-mockingbird-2_rm21xu",
+    "to-kill-a-mockingbird-3_u4f1kf"
+  ],
+  "Books & Music|East of Eden": [
+    "east-of-eden-1_bshxlh",
+    "east-of-eden-2_ybir8j",
+    "east-of-eden-3_ud2oke"
+  ],
+  "Books & Music|Moby-Dick": [
+    "moby-dick-1_jwainh",
+    "moby-dick-2_srvvwa",
+    "moby-dick-3_vl4kww"
+  ],
+  "Books & Music|One Hundred Years of Solitude": [
+    "one-hundred-years-of-solitude-1_tz4lgh",
+    "one-hundred-years-of-solitude-2_fzu0qm",
+    "one-hundred-years-of-solitude-3_nvwdx6"
+  ],
+  "Books & Music|The Old Man and the Sea": [
+    "the-old-man-and-the-sea-1_erlhew",
+    "the-old-man-and-the-sea-2_vrffj8",
+    "the-old-man-and-the-sea-3_z282ta"
+  ],
+  "Books & Music|Celtic Punk": [
+    "celtic-punk-1_itay1r",
+    "celtic-punk-2_axiqbi",
+    "celtic-punk-3_e5qxqr"
+  ],
+  "Books & Music|Classic Rock": [
+    "classic-rock-1_b0ikqf",
+    "classic-rock-2_s16v2f",
+    "classic-rock-3_z9mbch"
+  ],
+  "Books & Music|Island Music": [
+    "island-music-1_atazdc",
+    "island-music-2_kielrd",
+    "island-music-3_f2ybzb"
+  ]
+}
